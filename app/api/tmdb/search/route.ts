3d614baffa8641f2d807dcbac43a7b3e9cba3ee4
Logan Joecks <ljoecks@gmail.com>
2024-06-10 16:43:02 -0400

    fix route.ts

--- app/api/tmdb/search/route.ts
+++ app/api/tmdb/search/route.ts
@@ -1,5 +1,6 @@
 import { searchMedia } from '@/lib/TmdbService'
 import { MediaType } from '@prisma/client'
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request, { params }: { params: any }) {
   const { searchParams } = new URL(request.url)
@@ -7,10 +8,10 @@ export async function GET(request: Request, { params }: { params: any }) {
   const query = searchParams.get('query')
 
   if (!mediaType || !query) {
-    return Response.json({ data: { results: [] } })
+    return NextResponse.json({ data: { results: [] } })
   }
 
   const data = await searchMedia(mediaType as MediaType, query)
 
-  return Response.json({ data })
+  return NextResponse.json({ data })
 }
