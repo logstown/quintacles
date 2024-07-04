bad12a429f4dbbe0f9162724482d5938f153aff8
Logan Joecks <ljoecks@gmail.com>
2024-07-04 14:37:55 -0400

    add blurry-blob

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { MediaTypeUserLists } from '@/components/MediaTypeUserLists'
+import BlurryBlob from '@/components/animata/background/blurry-blob'
 import { FlipWords } from '@/components/flip-words'
 import { Button } from '@nextui-org/button'
 import { PlusIcon } from 'lucide-react'
@@ -23,6 +24,13 @@ export default async function Home() {
 
   return (
     <>
+      <div className='absolute left-[40%] top-[40%]'>
+        <BlurryBlob
+          className='opacity-20'
+          firstBlobColor='bg-primary-400'
+          secondBlobColor='bg-secondary-400'
+        />
+      </div>
       <div className='mx-auto flex max-w-screen-lg flex-col items-center justify-around p-10 md:flex-row'>
         <div className='drop-shadow-2xl'>
           <div className='flex flex-wrap items-center justify-center gap-2'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'pop-blob': {
+          '0%': { transform: 'scale(1)' },
+          '33%': { transform: 'scale(1.2)' },
+          '66%': { transform: 'scale(0.8)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        colors: {
+          filter: {
+            'blur-20': 'blur(20px)',
+            'blur-25': 'blur(25px)',
+          },
+        },
+      },
     },
   },
   darkMode: 'class',
