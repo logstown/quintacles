0bdc190cca62ef9552ade0ff0c4dae5c2eef0b97
Logan Joecks <ljoecks@gmail.com>
2024-07-12 10:02:41 -0400

    make button for create on mediatypeuserlists

--- components/MediaTypeUserLists.tsx
+++ components/MediaTypeUserLists.tsx
@@ -3,6 +3,9 @@ import { MediaType } from '@prisma/client'
 import { Suspense } from 'react'
 import { UserListSkeleton } from './user-list/UserListSkeleton'
 import { MediaTypeQuery } from '@/app/user/_components/MediaTypeQuery'
+import { Button } from '@nextui-org/button'
+import Link from 'next/link'
+import { PlusIcon } from 'lucide-react'
 
 export function MediaTypeUserLists({
   userId,
@@ -12,19 +15,33 @@ export function MediaTypeUserLists({
   username?: string
 }) {
   return (
-    <div className='mx-auto mt-10 flex max-w-screen-lg flex-col gap-20 md:mt-16'>
+    <div className='mx-auto mt-10 flex max-w-screen-lg flex-col gap-8 md:mt-16'>
       {mediaTypeArrForLists.map(mediaType => {
         const isEpisodes = mediaType.key === MediaType.TvEpisode
 
         return (
           <div
             key={mediaType.key}
-            className={`flex flex-col gap-4 sm:gap-6 ${isEpisodes ? '' : 'max-w-screen-lg'}`}
+            className={`flex flex-col gap-2 sm:gap-4 ${isEpisodes ? '' : 'max-w-screen-lg'}`}
           >
-            <h1 className='pl-4 font-bold capitalize md:text-2xl'>
-              <span className='text-foreground-400'>Recent </span>
-              {mediaTypes[mediaType.key].display}
-            </h1>
+            <div className='flex items-baseline gap-2'>
+              <h1 className='pl-4 font-bold capitalize md:text-2xl'>
+                <span className='text-foreground-400'>Recent </span>
+                {mediaTypes[mediaType.key].display}
+              </h1>
+              <Button
+                color='primary'
+                size='lg'
+                radius='lg'
+                variant='light'
+                as={Link}
+                className='m-6'
+                href={`/create/criteriia/${mediaTypes[mediaType.key].urlPlural}`}
+                endContent={<PlusIcon size={15} />}
+              >
+                Create
+              </Button>
+            </div>
             <Suspense
               key={JSON.stringify({ mediaType: mediaType.key })}
               fallback={
