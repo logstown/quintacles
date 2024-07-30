3f8b3bb2b168f6823501271c60ae4785c418e8c2
Logan Joecks <ljoecks@gmail.com>
2024-07-30 16:55:53 -0400

    Revert "prisma adapter"
    This reverts commit 5eb99854daf8c3c2f04a8aa0abce4b047ef7b131.

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,15 @@
-import { neonConfig, Pool } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
-import dotenv from 'dotenv'
-import ws from 'ws'
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  dotenv.config()
-  neonConfig.webSocketConstructor = ws
-  const connectionString = `${process.env.POSTGRES_PRISMA_URL}`
-
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaNeon(pool)
-  return new PrismaClient({ adapter })
-}
+  return new PrismaClient();
+};
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
