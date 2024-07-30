5eb99854daf8c3c2f04a8aa0abce4b047ef7b131
Logan Joecks <ljoecks@gmail.com>
2024-07-30 16:51:21 -0400

    prisma adapter

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { neonConfig, Pool } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import ws from 'ws'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+  dotenv.config()
+  neonConfig.webSocketConstructor = ws
+  const connectionString = `${process.env.POSTGRES_PRISMA_URL}`
+
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaNeon(pool)
+  return new PrismaClient({ adapter })
+}
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma;
+export default prisma
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
