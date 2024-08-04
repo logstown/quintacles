e22df9a333ec1d05670c504cff11ea5911cccb92
Logan Joecks <ljoecks@gmail.com>
2024-08-04 13:59:01 -0400

    add loadings

---
+++ app/browse/[mediaTypePlural]/loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@nextui-org/skeleton'
+
+export default function Loading() {
+  return (
+    <div className='mx-auto flex w-full flex-col items-center gap-8 pb-20'>
+      <Skeleton>
+        <h1 className='rounded-lg text-6xl font-semibold'>Browse</h1>
+      </Skeleton>
+      <Skeleton className='h-[137.5px] w-full max-w-screen-lg rounded-lg' />
+    </div>
+  )
+}
---
+++ app/user/[username]/loading.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from '@nextui-org/skeleton'
+
+export default function Loading() {
+  return <Skeleton className='mx-auto h-[440px] w-full max-w-screen-xl rounded-xl' />
+}
