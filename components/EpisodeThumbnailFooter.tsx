04d577988a5bf52a1194a20c87fb057b28b7d6f9
Logan Joecks <ljoecks@gmail.com>
2024-08-05 17:25:49 -0400

    center little episode title

--- components/EpisodeThumbnailFooter.tsx
+++ components/EpisodeThumbnailFooter.tsx
@@ -9,7 +9,7 @@ export function EpisodeThumbnailFooter({
 }) {
   return (
     <CardFooter
-      className={`absolute bottom-0 z-10 overflow-hidden bg-slate-900/60 py-2 text-sm text-neutral-200 ${isSeasons ? 'justify-center' : ''}`}
+      className={`absolute bottom-0 z-10 justify-center overflow-hidden bg-slate-900/60 py-2 text-sm text-neutral-200`}
     >
       {children}
     </CardFooter>
