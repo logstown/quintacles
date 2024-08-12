aeddd42e3410ca8dd10bf22b6d533084920078d2
Logan Joecks <ljoecks@gmail.com>
2024-08-12 08:43:48 -0400

    Update EpisodeThumbnailFooter.tsx

--- components/EpisodeThumbnailFooter.tsx
+++ components/EpisodeThumbnailFooter.tsx
@@ -9,7 +9,7 @@ export function EpisodeThumbnailFooter({
 }) {
   return (
     <CardFooter
-      className={`absolute bottom-0 z-10 justify-center overflow-hidden bg-slate-900/60 py-2 text-sm text-neutral-200`}
+      className={`absolute bottom-0 z-10 justify-center overflow-hidden rounded-b-xl bg-slate-900/60 py-2 text-sm text-neutral-200`}
     >
       {children}
     </CardFooter>
