28b64715817ff27dd1dc2cfabf6a8adf348e4e04
Logan Joecks <ljoecks@gmail.com>
2024-10-06 08:27:36 -0400

    fix suggestion with no poster

--- components/build-list/AddListIdx.tsx
+++ components/build-list/AddListIdx.tsx
@@ -8,7 +8,7 @@ export function AddListIdx({
   disabled?: boolean
 }) {
   return (
-    <div className='relative'>
+    <div className='relative h-full w-full'>
       {idx >= 0 && !disabled && (
         <div
           className={`absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 rounded-full bg-foreground-100 py-1 text-4xl font-bold text-primary-300 md:text-6xl ${idx === 0 ? 'px-5' : 'px-4'}`}
