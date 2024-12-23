8b0ffe7c9b88a4c2fea227c6fc38dcc15a7c1f78
Logan Joecks <ljoecks@gmail.com>
2024-12-22 20:57:08 -0500

    update listloading

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,46 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import {
+  Besley,
+  Black_Ops_One,
+  Bree_Serif,
+  Cardo,
+  Chango,
+  Cherry_Swash,
+  Chicle,
+  Cinzel,
+  Copse,
+  Crimson_Pro,
+  Fira_Code as FontMono,
+  Inter as FontSans,
+  Galindo,
+  Girassol,
+  Leckerli_One,
+  Lemon,
+  Linden_Hill,
+  Modak,
+  Monoton,
+  Montagu_Slab,
+  Oregano,
+  Radley,
+  Shrikhand,
+  Slabo_13px,
+  Solway,
+  Special_Elite,
+  Tangerine,
+  Trade_Winds,
+  Young_Serif,
+} from 'next/font/google'
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-});
+  subsets: ['latin'],
+  variable: '--font-mono',
+})
+
+export const fancy = Cherry_Swash({
+  subsets: ['latin'],
+  weight: '400',
+})
