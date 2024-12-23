74ffb970aa8d5f471ad8b532e1674f4822709aba
Logan Joecks <ljoecks@gmail.com>
2024-12-22 20:57:58 -0500

    Revert "update listloading"
    This reverts commit 8b0ffe7c9b88a4c2fea227c6fc38dcc15a7c1f78.

--- app/list/_components/ListDetail.tsx
+++ app/list/_components/ListDetail.tsx
@@ -22,8 +22,6 @@ import { UserListInfinite } from '@/app/browse/_components/UserListInfinite'
 import { auth } from '@clerk/nextjs/server'
 import { getImages, getImageStuff } from '@/lib/TmdbService'
 import { UserListIcon } from '@/components/user-list/UserListIcon'
-import clsx from 'clsx'
-import { fancy } from '@/config/fonts'
 
 export type ListDetailProps = { id: number } | { username: string; slug: string }
 
@@ -32,8 +30,6 @@ export async function ListDetail(props: ListDetailProps) {
 
   // const deal = await getImageStuff()
   // console.log(deal)
-
-  await new Promise(resolve => setTimeout(resolve, 2000))
 
   if (!userList || !userAddedAt) {
     notFound()
@@ -159,84 +155,78 @@ export async function ListDetail(props: ListDetailProps) {
         {listItemsReverse.map((item, i) => (
           <div
             key={item.tmdbId}
-            className='flex aspect-video w-full items-end justify-center rounded-xl bg-cover bg-center px-6 pb-6 pt-52 shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] sm:px-16 sm:pb-16'
+            className='flex aspect-video w-full flex-col items-center justify-end rounded-xl bg-cover bg-center px-6 pb-6 pt-52 shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] sm:px-16 sm:pb-16'
             style={{
               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 1)),url(${item.backdropUrl})`,
             }}
           >
-            <div className='flex flex-col items-center justify-around sm:flex-row sm:items-baseline'>
-              <h2 className='z-30 text-7xl text-neutral-200 drop-shadow-2xl'>
-                {5 - i}
-              </h2>
-              <div className='flex max-w-prose flex-col gap-10'>
-                <div
-                  className={`flex justify-center gap-6 sm:gap-10 lg:gap-12 ${isEpisodes ? 'items-start' : `${item.logoPath ? 'items-center' : 'items-end'}`}`}
-                >
-                  {/* <h2
+            <div className='flex max-w-prose flex-col gap-6'>
+              <div
+                className={`flex gap-6 sm:gap-10 lg:gap-12 ${isEpisodes ? 'items-start' : `${item.logoPath ? 'items-center' : 'items-end'}`}`}
+              >
+                <h2
                   className={`text-5xl font-extrabold !leading-[.8] tracking-tight text-neutral-100 drop-shadow-[0_1px_1px_white] lg:text-7xl`}
                 >
                   {5 - i}
-                </h2> */}
-                  <div
-                    style={{ color: item.textColor }}
-                    className='flex flex-col justify-center gap-2 sm:gap-3'
+                </h2>
+                <div
+                  style={{ color: item.textColor }}
+                  className='flex flex-col justify-center gap-2 sm:gap-3'
+                >
+                  <ListItemLink
+                    mediaType={restrictions.mediaType}
+                    tvShowId={restrictions.EpisodesTvShow.id}
+                    item={item}
                   >
-                    <ListItemLink
-                      mediaType={restrictions.mediaType}
-                      tvShowId={restrictions.EpisodesTvShow.id}
-                      item={item}
-                    >
-                      <Tooltip content={item.name} delay={1000}>
-                        {item.logoPath ? (
-                          <img className='drop-shadow-xl' src={item.logoPath} />
-                        ) : (
-                          <h1 className='line-clamp-4 overflow-visible text-balance text-3xl font-extrabold !leading-[.8] tracking-tight drop-shadow-2xl sm:text-5xl'>
-                            {item.name}{' '}
-                            {!isEpisodes &&
-                              (restrictions.mediaType === MediaType.TvShow ||
-                                !restrictions.year ||
-                                restrictions.year > 10000) && (
-                                <small className='text-[40%] font-medium'>
-                                  {new Date(item.date).getFullYear()}
-                                </small>
-                              )}
-                          </h1>
-                        )}
-                      </Tooltip>
-                    </ListItemLink>
-                    {isEpisodes && (
-                      <h3 className='flex flex-col flex-wrap items-baseline sm:flex-row sm:gap-4 sm:text-xl md:text-2xl'>
-                        <p>
-                          Season <span className='font-bold'>{item.seasonNum}</span>{' '}
-                          · Episode{' '}
-                          <span className='font-bold'>{item.episodeNum}</span>
-                        </p>
-                        <p className='text-tiny sm:text-sm md:text-base lg:text-tiny xl:text-base'>
-                          {format(new Date(item.date), 'MMM d, yyyy')}
-                        </p>
-                      </h3>
-                    )}
-                  </div>
-                  {!includes(item.backdropUrl, 'tmdb') && (
-                    <Image
-                      unoptimized
-                      isBlurred
-                      alt={`${item.name} poster`}
-                      classNames={{
-                        img: 'max-h-[150px] max-w-[100px] sm:max-h-[277.5px] sm:max-w-[185px]',
-                        blurredImg:
-                          'max-h-[150px] max-w-[100px] sm:max-h-[277.5px] sm:max-w-[185px]',
-                      }}
-                      as={NextImage}
-                      height={513}
-                      width={342}
-                      src={getTmdbImageUrl(item.posterPath, 'w342')}
-                    />
+                    <Tooltip content={item.name} delay={1000}>
+                      {item.logoPath ? (
+                        <img className='drop-shadow-xl' src={item.logoPath} />
+                      ) : (
+                        <h1 className='line-clamp-4 overflow-visible text-balance text-3xl font-extrabold !leading-[.8] tracking-tight drop-shadow-2xl sm:text-5xl'>
+                          {item.name}{' '}
+                          {!isEpisodes &&
+                            (restrictions.mediaType === MediaType.TvShow ||
+                              !restrictions.year ||
+                              restrictions.year > 10000) && (
+                              <small className='text-[40%] font-medium'>
+                                {new Date(item.date).getFullYear()}
+                              </small>
+                            )}
+                        </h1>
+                      )}
+                    </Tooltip>
+                  </ListItemLink>
+                  {isEpisodes && (
+                    <h3 className='flex flex-col flex-wrap items-baseline sm:flex-row sm:gap-4 sm:text-xl md:text-2xl'>
+                      <p>
+                        Season <span className='font-bold'>{item.seasonNum}</span> ·
+                        Episode <span className='font-bold'>{item.episodeNum}</span>
+                      </p>
+                      <p className='text-tiny sm:text-sm md:text-base lg:text-tiny xl:text-base'>
+                        {format(new Date(item.date), 'MMM d, yyyy')}
+                      </p>
+                    </h3>
                   )}
                 </div>
-                <div className='text-sm font-light text-white sm:text-base'>
-                  <ItemOverview omitNoOverview overview={item.overview ?? ''} />
-                </div>
+                {!includes(item.backdropUrl, 'tmdb') && (
+                  <Image
+                    unoptimized
+                    isBlurred
+                    alt={`${item.name} poster`}
+                    classNames={{
+                      img: 'max-h-[150px] max-w-[100px] sm:max-h-[277.5px] sm:max-w-[185px]',
+                      blurredImg:
+                        'max-h-[150px] max-w-[100px] sm:max-h-[277.5px] sm:max-w-[185px]',
+                    }}
+                    as={NextImage}
+                    height={513}
+                    width={342}
+                    src={getTmdbImageUrl(item.posterPath, 'w342')}
+                  />
+                )}
+              </div>
+              <div className='text-sm font-light text-white sm:text-base'>
+                <ItemOverview omitNoOverview overview={item.overview ?? ''} />
               </div>
             </div>
           </div>
--- app/list/_components/ListLoading.tsx
+++ app/list/_components/ListLoading.tsx
@@ -4,23 +4,25 @@ export default function ListLoading() {
   const arr = [1, 2, 3, 4, 5]
   return (
     <main>
-      <div className='mx-8 mb-14 flex flex-col items-center gap-4'>
-        <Skeleton className='h-22 w-[50%] rounded-lg text-center text-5xl font-semibold capitalize tracking-tight sm:text-6xl lg:text-7xl'>
+      <div className='mx-8 mb-8 flex flex-col items-center gap-4'>
+        <Skeleton className='w-[60%] rounded-lg text-center text-5xl font-semibold capitalize tracking-tight sm:text-6xl lg:text-7xl'>
           dummy
         </Skeleton>
         <div className='flex flex-none items-center gap-3'>
           <div>
             <Skeleton className='flex h-12 w-12 rounded-full' />
           </div>
-          <div className='flex flex-col gap-2'>
+          <div>
             <Skeleton className='h-3 w-[180px] rounded-lg' />
-            <Skeleton className='h-3 w-[120px] rounded-lg' />
           </div>
         </div>
       </div>
-      <div className='mx-auto flex max-w-screen-lg flex-col items-center gap-10'>
+      <div className='mx-auto flex max-w-screen-2xl flex-col items-center gap-10'>
         {arr.map(i => (
-          <Skeleton className='aspect-video w-full rounded-xl' key={i} />
+          <Skeleton
+            className='aspect-[672/587] w-full max-w-screen-sm rounded-xl lg:aspect-[26.66666/9] lg:w-full lg:max-w-none'
+            key={i}
+          />
         ))}
       </div>
     </main>
