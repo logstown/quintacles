49d5510db5bafe4acb93485c0fb9ac9336edd940
Logan Joecks <ljoecks@gmail.com>
2025-11-17 12:56:30 -0500

    Update SuggestionItem.tsx

--- components/build-list/SuggestionItem.tsx
+++ components/build-list/SuggestionItem.tsx
@@ -1,10 +1,10 @@
 import { AddListIdx } from './AddListIdx'
 import { getTmdbImageUrl } from '../../lib/random'
 import { ListItem } from '@prisma/client'
-import { Card } from "@heroui/card"
-import { Tooltip } from "@heroui/tooltip"
+import { Card } from '@heroui/card'
+import { Tooltip } from '@heroui/tooltip'
 import NextImage from 'next/image'
-import { Image } from "@heroui/image"
+import { Image } from '@heroui/image'
 
 export function SuggestionItem({
   item,
@@ -20,7 +20,7 @@ export function SuggestionItem({
   const isChosen = idx >= 0
 
   return (
-    (<Tooltip
+    <Tooltip
       content={`${item.name} (${new Date(item.date).getFullYear()})`}
       placement='bottom'
       isDisabled={!item.posterPath}
@@ -30,16 +30,16 @@ export function SuggestionItem({
         isPressable
         isDisabled={isChosen}
         onPress={() => onItemSelected(item)}
-        className={`justify-center shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] ${idx < 0 ? 'hover:z-10 hover:scale-110 hover:shadow-lg' : ''} ${isChosen ? 'cursor-pointer' : ''}`}
+        className={`justify-center shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] ${isChosen ? 'cursor-pointer' : 'hover:z-10 hover:scale-110 hover:shadow-lg'}`}
       >
         <AddListIdx idx={idx}>
           {item.posterPath ? (
             // TODO: add NextImage back maybe
-            (<Image
+            <Image
               width={300}
               src={getTmdbImageUrl(item.posterPath, 'w342')}
               alt={`${item.name} poster`}
-            />)
+            />
           ) : (
             <div className='flex h-full w-full flex-col items-center justify-center rounded-md bg-slate-300 p-4'>
               <div className='mb-4'>{mediaTypeIcon}</div>
@@ -49,6 +49,6 @@ export function SuggestionItem({
           )}
         </AddListIdx>
       </Card>
-    </Tooltip>)
-  );
+    </Tooltip>
+  )
 }
