f4a45e008460b52fb3184188639fcdcaca5c2f60
Logan Joecks <ljoecks@gmail.com>
2025-12-07 12:54:43 -0500

    little visual changes to listdetailitem

--- app/list/_components/ListDetailItem.tsx
+++ app/list/_components/ListDetailItem.tsx
@@ -27,24 +27,24 @@ export function ListDetailItem({
     <div
       className={`relative w-full border border-neutral-800/50 shadow-[0_2.8px_2.2px_rgba(0,0,0,0.034),0_6.7px_5.3px_rgba(0,0,0,0.048),0_12.5px_10px_rgba(0,0,0,0.06),0_22.3px_17.9px_rgba(0,0,0,0.072),0_41.8px_33.4px_rgba(0,0,0,0.086),0_100px_80px_rgba(0,0,0,0.12)] ${isModal ? 'lg:rounded-xl' : 'rounded-xl'}`}
     >
-      <div className='absolute -left-4 -top-4 z-10 flex size-16 items-center justify-center rounded-full bg-linear-to-br from-foreground-100 to-foreground-300 shadow-[0_4px_12px_rgba(0,0,0,0.4),0_0_0_2px_rgba(255,255,255,0.1)] sm:size-20'>
-        <span className='font-mono text-3xl font-extrabold leading-none tracking-tight text-foreground-900 sm:text-4xl'>
+      <div className='from-foreground-100 to-foreground-300 absolute -top-4 -left-4 z-10 flex size-16 items-center justify-center rounded-full bg-linear-to-br shadow-[0_4px_12px_rgba(0,0,0,0.4),0_0_0_2px_rgba(255,255,255,0.1)] sm:size-20'>
+        <span className='text-foreground-900 font-mono text-3xl leading-none font-extrabold tracking-tight sm:text-4xl'>
           {5 - i}
         </span>
       </div>
       <div
-        className={`flex aspect-video w-full flex-col items-center justify-end bg-cover bg-center px-6 ${isModal ? 'lg:rounded-t-xl' : 'rounded-t-xl'} `}
+        className={`flex aspect-video w-full flex-col items-center justify-end bg-cover bg-center px-12 ${isModal ? 'lg:rounded-t-xl' : 'rounded-t-xl'} `}
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0), ${item.bgColor}),url(${item.backdropUrl})`,
         }}
       >
         <div className='-mb-52 flex max-w-prose flex-col gap-6 sm:gap-10'>
           <div
-            className={`flex gap-6 sm:gap-10 lg:gap-12 ${item.logoPath ? 'items-center justify-center' : isEpisodes ? 'items-center' : 'items-end'}`}
+            className={`flex justify-center gap-6 sm:gap-10 lg:gap-12 ${item.logoPath ? 'items-center' : isEpisodes ? 'items-center' : 'items-end'}`}
           >
             <div
               style={{ color: item.textColor }}
-              className='flex flex-col justify-center gap-3 truncate'
+              className='flex flex-col items-center justify-center gap-3 truncate'
             >
               <ListItemLink
                 mediaType={restrictions.mediaType}
@@ -54,12 +54,15 @@ export function ListDetailItem({
                 <Tooltip content={item.name} delay={1000}>
                   {item.logoPath ? (
                     <img
-                      className='max-h-52 w-full drop-shadow-xl'
+                      className='max-h-52 w-full'
+                      style={{
+                        filter: 'drop-shadow(8px 8px 16px rgba(0, 0, 0, 0.5))',
+                      }}
                       src={item.logoPath}
                     />
                   ) : (
                     <span
-                      className={`line-clamp-4 overflow-visible text-balance text-4xl font-extrabold leading-none tracking-tight drop-shadow-2xl sm:text-6xl`}
+                      className={`line-clamp-4 pb-1.5 text-center text-4xl leading-none font-extrabold tracking-tight text-balance drop-shadow-2xl sm:text-6xl`}
                     >
                       {item.name}{' '}
                       {!isEpisodes &&
@@ -80,7 +83,7 @@ export function ListDetailItem({
                     Season <span className='font-bold'>{item.seasonNum}</span> ·
                     Episode <span className='font-bold'>{item.episodeNum}</span>
                   </p>
-                  <p className='text-tiny leading-[.8] sm:text-sm md:text-base lg:text-tiny xl:text-base'>
+                  <p className='text-tiny lg:text-tiny leading-[.8] sm:text-sm md:text-base xl:text-base'>
                     {format(new Date(item.date), 'MMM d, yyyy')}
                   </p>
                 </h3>
